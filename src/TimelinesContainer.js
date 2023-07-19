65259c2eb866264499b733a4f0bd8e03641314e2
Lasha <lasha84@gmail.com>
2023-07-19 03:37:59 -0400

    Add timestamp toggle, refresh, infinite scroll, etc.

--- src/TimelinesContainer.js
+++ src/TimelinesContainer.js
@@ -15,6 +15,18 @@ const simulateAPICall = (timeout) =>
     }, timeout);
   });
 
+const simulateLoadOlderEntries = (timeout) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      const entries = Array.from({ length: 5 }, (_, entryIndex) => ({
+        id: entryIndex,
+        content: `Older Entry ${entryIndex}`,
+        timestamp: new Date().toString(),
+      })).reverse();
+      resolve(entries);
+    }, timeout);
+  });
+
 const TimelinesContainer = () => {
   const timelineCount = 10; // Number of timelines
   const [loading, setLoading] = useState(true);
@@ -61,12 +73,37 @@ const Timeline = ({ title, entries }) => {
   const [timelineEntries, setTimelineEntries] = useState(entries);
   const [loading, setLoading] = useState(false);
   const [showTimestamp, setShowTimestamp] = useState(true);
+  const [newEntriesLoaded, setNewEntriesLoaded] = useState(false);
 
   const timelineRef = useRef(null);
+  const prevScrollHeightRef = useRef(0);
 
   useEffect(() => {
-    scrollToBottom();
-  }, [timelineEntries]); // Trigger the effect whenever timelineEntries change
+    if (!newEntriesLoaded) {
+      scrollToBottom();
+    } else {
+      const newContentHeight =
+        timelineRef.current.scrollHeight - prevScrollHeightRef.current;
+      timelineRef.current.scrollTop += newContentHeight;
+      prevScrollHeightRef.current = timelineRef.current.scrollHeight;
+      setNewEntriesLoaded(false);
+    }
+  }, [timelineEntries]);
+
+  useEffect(() => {
+    prevScrollHeightRef.current = timelineRef.current.scrollHeight;
+    const handleScroll = () => {
+      if (timelineRef.current.scrollTop === 0) {
+        loadOlderPosts();
+      }
+    };
+
+    timelineRef.current.addEventListener('scroll', handleScroll);
+
+    return () => {
+      timelineRef.current.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   const addEntry = (event) => {
     event.preventDefault();
@@ -94,6 +131,7 @@ const Timeline = ({ title, entries }) => {
     try {
       const refreshedEntries = await simulateAPICall(1000);
       setTimelineEntries(refreshedEntries);
+      setNewEntriesLoaded(true);
     } catch (error) {
       console.error('Error refreshing data:', error);
     } finally {
@@ -103,6 +141,19 @@ const Timeline = ({ title, entries }) => {
 
   const handleToggleTimestamp = () => {
     setShowTimestamp((prevShowTimestamp) => !prevShowTimestamp);
+  };
+
+  const loadOlderPosts = async () => {
+    setLoading(true);
+    try {
+      const olderEntries = await simulateLoadOlderEntries(1000);
+      setTimelineEntries((prevEntries) => [...olderEntries, ...prevEntries]);
+      setNewEntriesLoaded(true);
+    } catch (error) {
+      console.error('Error loading older posts:', error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
